67356891010b3517af3ebd948bf5da52e98cd01f
sandra <sandravhawkins@gmail.com>
2023-10-11 00:14:55 -0400

    vutils refactoring and testing

--- src/verb_utils.py
+++ src/verb_utils.py
@@ -1,155 +1,116 @@
 # functions and classes for verb utilities that the Verb classes often use
 
 import re
-# regex patterns
-long_vowel = "[áéíúůý]"
-consonant = "[bcčdďfghjklmnňpqrřsštťvwxzž]"
-neutral_consonant = "[bmpv]"
-soft_consonant = "[cčďjlňřšťž]"
-digraph = "(ch)|(st)|(št)|(ct)|(čt)"
 
+# regex patterns -> TODO: make these public? static members
+short_vowel = "[aeiouy]"
+long_vowel = "[áéíóúůý]|(ou)"
+soft_vowel = "[ěií]"
+hard_consonant = "[dghknrst]"
+neutral_consonant = "[bmpvfqwx]"
+soft_consonant = "[cčďjlňřšťzž]"
+digraph = "(ch)|(st)|(št)|(ct)|(čt)"
+consonant = hard_consonant + "|" + neutral_consonant + "|" + soft_consonant + "|" + digraph
+vowel = short_vowel + "|" + long_vowel + "|" + soft_vowel
+
+# dictionaries for letter mappings -> TODO: make these private static members
+hard_to_soft = {"k":"c", "d":"ď", "g":"z", "h":"z", "n":"ň", "r":"ř", "ch":"š", "t":"ť"}
+soft_to_hard = {"c":"k", "ď":"d","z":"h", "ň":"n", "ř":"r", "š":"ch", "ť":"t"}
+long_to_short = {"á":"a", "é":"e", "í":"i", "ů":"o", "ou":"u", "ý":"y"}
+short_to_long = {"a":"á", "e":"é", "i":"í", "o":"ů", "u":"ou", "y":"ý"}
+
+# helper functions -> TODO: make these private members
 def italics(string):
 	return "\x1B[3m" + string + "\x1B[23m"
 
+def get_val_from_dict(d, key):
+	'''function that retrieves value at <key> from <map> via get(). defaults to <key> as value.'''
+	return d.get(key, key)
 
-#### functions to change/alter spellings ####
+# functions to change specific letters/digraphs
+def get_short_vowel(long_vowel):
+	'''retrieves the corresponding short vowel of <long_vowel>'''
+	return get_val_from_dict(long_to_short, long_vowel)
 
-### vowel functions ###
-def get_short_vowel(_long_vowel):
-	"""returns the short vowel form of given long vowel"""
-	ret = ""
-	if _long_vowel == "á":
-		ret = "a"
-	elif _long_vowel == "é":
-		ret == "e"
-	elif _long_vowel == "í":
-		ret == "i"
-	elif _long_vowel == "ý":
-		ret = "y"
-	elif _long_vowel == "ú" or _long_vowel == "ů" or _long_vowel == "ou":
-		ret = "u"
-	# FIXME: "Ů" is a long vowel for O, not U.
-	# BUG: add case for not long vowel -> nothing should happen
-	# WHERE'S get_long_vowel()?
-	return ret
+def get_long_vowel(short_vowel):
+	'''retrieves the corresponding long vowel of <short vowel>'''
+	return get_val_from_dict(short_to_long, short_vowel)
+
+def get_hard_consonant(soft_consonant):
+	'''retrieves the corresponding hard consonant of <soft_consonant>'''
+	# FIXME: there is ambiguity from key 'z' since it can lead to either hard g or h. for now it is z->h only.
+	return get_val_from_dict(soft_to_hard, soft_consonant)
+
+def get_soft_consonant(hard_consonant):
+	'''retrieves the corresponding soft consonant of <hard_consonant>'''
+	return get_val_from_dict(hard_to_soft, hard_consonant)
 
 def isvowel(letter):
-	"""determines if given letter is a czech vowel (can be digraph)"""
-	vowel = False
-	if re.search("([aáeéěiíoóuúůyý]|(ou))", letter):
-		vowel = True
-	return vowel
+	'''determines if <letter> is a vowel'''
+	return re.search(vowel, letter) != None
+
+def isconsonant(letter):
+	'''determines if <letter> is a consonant'''
+	return re.search(consonant, letter) != None
 
 def get_vowel(stem):
-	"""returns the list of czech vowels contained within the stem string (as a string)"""
-	# TODO: this can be refactored with list comprehension
-	vowel_list = list("")
-	for i in range(len(stem) -1 , 0, -1):
-		if isvowel(stem[i]):
-			vowel_list.insert(0, stem[i])
-	vowel = ""
-	for i in range(0, len(vowel_list)):
-		x += vowel_list[i]
-	return x
+	'''returns the contained vowels in string <stem> as a list->string'''
+	return str(''.join([letter for letter in stem if isvowel(letter)]))
+
+def get_consonant(stem):
+	'''returns the contained consonants in string <stem> as a list->string'''
+	return str(''.join([letter for letter in stem if isconsonant(letter)]))
 
 def contains_vowel(string):
-	"""determines whether a given string contains a czech vowel (long or short)"""
-	# TODO: this can be refactored into a single regex statement
-	has_vowel = False
-	for i in range(0, len(string)):
-		if isvowel(string[i]):
-			has_vowel = True
-	return has_vowel
+	'''determines whether <string> contains czech vowels'''
+	#return re.search("(" + vowel + ")", string) != None
+	return get_vowel(string) != ""
 
 def lengthen(stem):
-	""""lengthens the provided short stem vowel, returning the modified string"""
-	# TODO: this can be refactored with regex replace and using lengthen(short vowel)
+	'''lengthens short vowel in <stem>'''
+	# FIXME: this can be improved to not only work for vowel-final strings.
+	# consonants at the end of the word
+	# more than one vowel...
 	vowel = get_vowel(stem)
-	new_stem = stem[:-len(vowel)]
-	new_vowel = ""
-	if vowel == "a":
-		new_vowel = "á"
-	elif vowel == "e":
-		new_vowel = "é"
-	elif vowel == "i":
-		new_vowel = "í"
-	elif vowel == "o":
-		new_vowel = "ů"
-	elif vowel == "u":
-		new_vowel = "ou"
-	elif vowel == "y":
-		new_vowel = "ý"
-	new_stem += new_vowel
-	return new_stem
+	stem_no_vowel = stem[:-len(vowel)] if len(vowel) > 0 else stem[:len(stem)]
+	return stem_no_vowel + get_long_vowel(vowel)
 
 def shorten(stem):
-	"""shortens the provided long stem vowel, returning the modified string"""
-	# TODO: this can be refactored with regex replace and shorten(long vowel)
+	'''shortens long vowel in <stem>'''
+	# FIXME: this can be improved to not only work for vowel-final strings.
+	# consonants at the end of the word
+	# more than one vowel...
 	vowel = get_vowel(stem)
-	new_stem = stem[:-len(vowel)]
-	new_vowel = ""
-	if vowel == "á":
-		new_vowel = "a"
-	elif vowel == "é":
-		new_vowel = "e"
-	elif vowel == "í":
-		new_vowel = "i"
-	elif vowel == "ou":
-		list(new_stem).pop() # stem was 'no' instead of just 'n'
-		new_vowel = "u"
-	elif vowel == "ů":
-		new_vowel = "o"
-	elif vowel == "ý":
-		new_vowel = "y"
-	new_stem += new_vowel
-	return new_stem
+	stem_no_vowel = stem[:-len(vowel)] if len(vowel) > 0 else stem[:len(stem)]
+	return stem_no_vowel + get_short_vowel(vowel)
 
-
-### consonant functions ###
 def soften(stem):
-	"""softens the (hard) stem by adding the háček"""
-	# TODO: refactor to pass in only the final stem consonant (make 2 functions)
-	new_stem = ""
-	if stem[-1] == "t":
-		new_stem = stem[:-1] + "ť"
-	elif stem[-1] == "d":
-		new_stem = stem[:-1] + "ď"
-	elif stem[-1] == "n":
-		new_stem = stem[:-1] + "ň"
-	else:
-		new_stem = stem
-	return new_stem
+	'''softens the final hard consonant in <stem>'''
+	# FIXME: this can be improved upon
+	consonant = get_consonant(stem)
+	stem_no_consonant = stem[:-len(consonant)] if len(consonant) > 0 else stem[:len(stem)]
+	return stem_no_consonant + get_soft_consonant(consonant)
 
 def harden(stem):
-	"""hardens the (soft) stem by adding the háček"""
-	# TODO: refactor to pass in only the final stem consonant (make 2 functions)
-	new_stem = ""
-	if stem[-1] == "ť":
-		new_stem = stem[:-1] + "t"
-	elif stem[-1] == "ď":
-		new_stem = stem[:-1] + "d"
-	elif stem[-1] == "ň":
-		new_stem = stem[:-1] + "n"
-	else:
-		new_stem = stem
-	return new_stem
+	'''hardens the final soft consonant in <stem>'''
+	# FIXME: this can be improved upon
+	consonant = get_consonant(stem)
+	stem_no_consonant = stem[:-len(consonant)] if len(consonant) > 0 else stem[:len(stem)]
+	return stem_no_consonant + get_hard_consonant(consonant)
 
 def fix_spelling(word):
-	"""fixes spelling of word with e or ě after hardening or softening of final stem consonant"""
-	# TODO: can be refactored to use previously defined functions, and better naming
-	# soft consonants and ě, i, or í
-	x = re.findall("([cčďjlňřsšťzž][ěií])", word)
-	if x == []:
-		return word
-	first_letter = x[0][0]
-	second_letter = "e" if x[0][1] == "ě" else x[0][1]
-	if first_letter == "ď":
-		first_letter = "d"
-	elif first_letter == "ť":
-		first_letter = "t"
-	elif first_letter == "ň":
-		first_letter = "n"
-	word = re.sub(x[0], first_letter + second_letter, word)
+	'''fixes spelling of soft consonants ď, ť, and ň  aside soft vowels i, í, and ě in <word>'''
+	if (soft_matches := re.findall("(" + soft_consonant + soft_vowel + ")", word)):
+		for match in soft_matches:
+			# make e->ě if not preceded by ď, ť, or ň
+			consonant = match[0]
+			vowel = "e" if match[1]== "ě" else match[1]
+			
+			if re.search("[ďťň]", match[0]):
+				consonant = harden(match[0])
+				vowel = match[1]
+			# replace consonant and vowel
+			word = re.sub(match, consonant + vowel, word)
 	return word
 
 
--- test/__init__.py
+++ test/__init__.py
@@ -5,5 +5,5 @@
 
 # conjugator.py not included as that is more or less the 'main' program
 from src.verb_utils import * # TODO: make verb_utils be a class instead of a series of functions
-from src.verbs import * # Imports Verb classes: base Verb and all derived (a lot)
-from src.conjugator_utils import * # TODO: make conjugator_utils be a class instead of a series of functions
+#from src.verbs import * # Imports Verb classes: base Verb and all derived (a lot)
+#from src.conjugator_utils import * # TODO: make conjugator_utils be a class instead of a series of functions
